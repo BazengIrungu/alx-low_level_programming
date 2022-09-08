a1d0f8feccb2e026f1480f2dcf688d339cac68a5
Bazeng Irungu <duncanmainairungu@gmail.com>
2022-09-09 02:49:23 +0300

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point
+*printf task
+*return: 0 after execution
+*/
+int main(void)
+{
+printf("With proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
