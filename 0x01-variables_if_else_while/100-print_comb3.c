1d77901d96a89aabc52bf82352d86d858de332a0
Bazeng Irungu <duncanmainairungu@gmail.com>
2022-09-13 00:54:20 +0300

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: Always 0(success)
+  */
+
+int main(void)
+
+{
+	int x,y;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+		for (y = x + 1; y <= '9'; y++)
+
+		{
+			putchar(x);
+			putchar(y);
+
+			if (x == '8' && y == '9' )
+				;
+			else 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+
+}
